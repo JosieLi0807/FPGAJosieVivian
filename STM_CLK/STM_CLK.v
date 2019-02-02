079a58f765f41211f2ccf0d6f5e460ee1b630a4d
Josie Li <33377879+JosieLi0807@users.noreply.github.com>
2019-02-02 14:01:30 -0500

    took so long

--- STM_CLK/STM_CLK.v
+++ STM_CLK/STM_CLK.v
@@ -4,6 +4,7 @@ module STM_CLK (clock_in, clock_out, clock_out_div2, clock_out_div3_33, clock_ou
 	output clock_out, clock_out_div2, clock_out_div3_33, clock_out_div3_50, clock_pll;
 	
 	reg[1:0] count = 2'd0;
+	reg[0:0] fedge_msb = 1'd0;
 	
 	//clock_out is a copy of clock_in (to display as reference on the oscilloscope)
 	assign clock_out = clock_in;
@@ -25,17 +26,18 @@ module STM_CLK (clock_in, clock_out, clock_out_div2, clock_out_div3_33, clock_ou
 			clock_out_div3_33 <= ~clock_out_div3_33;
 		end
 	count <= count + 1;
-	if (count == 2'd3)
+	if (count == 2'd2)
 		begin
 			count <= 2'd0;
 		end
 	end
-	
 	// assign clock_out_div3_33 = 0;
 
 	//clock division by 3 with 50% duty cycle
-	// TODO
-	assign clock_out_div3_50 = 0;
+	always @ (negedge clock_in) begin
+		fedge_msb = count[1];
+	end
+	assign clock_out_div3_50 = fedge_msb | count[1];
 
 	//clock division using the Altera PLL IP
 	// TODO
